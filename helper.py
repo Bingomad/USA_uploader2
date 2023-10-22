c1d32cdba1c2c38438aa9897ad1c72bf73701913
pjvip <140431425+pjvip@users.noreply.github.com>
2023-10-22 13:59:59 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -198,15 +198,11 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     start_time = time.time()
 
     try:
-        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) #, progress=progress_bar,progress_args=(reply,start_time))
+        await m.reply_video(filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur, progress=progress_bar,progress_args=(reply,start_time))
         await copy.copy(chat_id = LOG) 
-    except TimeoutError:
-        await asyncio.sleep(5) 
-        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) #, progress=progress_bar,progress_args=(reply,start_time))
-        await copy.copy(chat_id = LOG)       
     except Exception:
-        copy = await bot.send_video(chat_id=m.chat.id,video=filename,caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur) #progress=progress_bar,progress_args=(reply,start_time))
-        await copy.copy(chat_id = LOG)
+        await m.reply_document(filename,caption=cc, progress=progress_bar,progress_args=(reply,start_time))
+        await copy.copy(chat_id = LOG) 
     os.remove(filename)
 
     os.remove(f"{filename}.jpg")
