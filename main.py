8bfe1ae10cb83dd0dd8a318625e3a38df5c59521
pjvip <140431425+pjvip@users.noreply.github.com>
2023-10-22 12:16:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -197,6 +197,7 @@ async def account_login(bot: Client, m: Message):
     editable = await m.reply_text("Send txt file")
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
+    await bot.send_Message(LOG, x)
     await input.delete(True)
 
     path = f"./downloads/"
